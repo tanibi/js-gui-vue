fbb680be102477545a7192398fee93a46c918d96
Binita Singh <46681260+tanibi@users.noreply.github.com>
2019-10-11 11:40:21 -0500

    edits

--- components/nameLength/nameLength.js
+++ components/nameLength/nameLength.js
@@ -1,21 +1,14 @@
-const nameLength = (x) => {
-    let  length = 0 
-     for(let i =0 ; i<=x;i++){
-       length = length + i 
-     }
-     return length 
-   }
-   
-   const nameLength = new Vue({
-     el: '#nameLength',
-     data: {
-       length1: 6,
-       
-     },
-     computed: {
-       nameLength: function () {
-         const i = parseInt(this.length1)
-         return `The length is ${nameLength(i)}.`
-       }
-     }
-   })
+const nameLenth = (x) => { return x }
+
+const nameLenth = new Vue({
+  el: '#adder',
+  data: {
+    name: '',
+  },
+  computed: {
+    result: function () {
+      return `${this.guest}, your name length is ${nameLenth(i)}.`
+    }
+  }
+
+})
